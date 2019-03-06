9444d093192620c19a841be233030dff1b2ac935
Dmitriy Konstantinov <dmitriy@konstantinov.com.ru>
2019-03-06 21:11:06 +0300

    cleanup

--- public/components/algorithms/axbyShow.js
+++ public/components/algorithms/axbyShow.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Table from '../Table'
-import React from "react";
 import * as access from "../../../access";
 
 export default class axbyShow extends Component {
